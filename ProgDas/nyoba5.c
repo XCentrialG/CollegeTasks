18ab9756a612e6efeb17911fb8b245b33e72027a
XCentrialG <xcentrialg@gmail.com>
2022-10-21 01:45:31 +0700

    modified:   ProgDas/nyoba5 	modified:   ProgDas/nyoba5.c

--- ProgDas/nyoba5.c
+++ ProgDas/nyoba5.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
-void average(void);
+float average(float, float, float);
+int matematika(int a);
+int fisika(int);
+int kimia(int);
+int a, b, c;
 
 int main(int argc, char const *argv[])
 {
-    float avg, a, b, math, psy, chm;
-    printf("Masukkan Nilai mtk: ");
-    scanf("%f", &math);
-    printf("Masukkan Nilai fisika: ");
-    scanf("%f", &psy);
-    printf("Masukkan Nilai Kimia: ");
-    scanf("%f", &chm);
+    printf("Nilai mtk: ");
+    scanf("%d", &a);
+    printf("\n");
+    fisika(a);
+}
 
-    printf("Nilai rata-ratanya adalah %.1f\n", avg);
+int fisika(int a)
+{
+    printf("Nilai fisika: ");
+    scanf("%d", &b);
+    printf("\n");
+    kimia(b);
+}
+
+int kimia(int b)
+{
+    printf("Nilai kimia: ");
+    scanf("%d", &c);
+    printf("\n");
+    average(a, b, c);
+}
+
+float average(float a, float b, float c)
+{
+    float avg, sum;
+    sum = a + b + c;
+    avg = sum / 3;
+    printf("rata rata adalah %.1f", avg);
 }
