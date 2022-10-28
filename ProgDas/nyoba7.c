aa9eb7448a80dca574b8fc4933cf25890693a830
XCentrialG <xcentrialg@gmail.com>
2022-10-28 21:45:48 +0700

    new file:   ProgDas/nyoba7 	new file:   ProgDas/nyoba7.c 	ProgDas/iseng ah 	ProgDas/iseng ah.c 	ProgDas/iseng ah.py

---
+++ ProgDas/nyoba7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <caca_conio.h>
+#include <ctype.h>
+int main(int argc, char const *argv[])
+{
+    char string1[] = "KASUR RUSAK";
+    char string2[] = "MAKAN MALAM";
+
+    printf("%s\n%s", string1, strrev)
+    return 0;
+}
