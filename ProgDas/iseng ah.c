59a55554cb6dce18374df67a89740448cca892dc
XCentrialG <xcentrialg@gmail.com>
2022-11-01 08:22:31 +0700

    new file:   ProgDas/iseng ah 	new file:   ProgDas/iseng ah.c 	new file:   ProgDas/iseng ah.py

---
+++ ProgDas/iseng ah.c
@@ -0,0 +1,14 @@
+int main(int argc, char const *argv[])
+{
+    if (2 + 2 == 4)
+    {
+        printf("A");
+        printf("\n\n");
+    }
+    else
+    {
+        printf("B");
+        printf("\n\n");
+    }
+    return 0;
+}
